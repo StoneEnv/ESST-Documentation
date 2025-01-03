465866e54ed5f71acef9e3cee9abad13811e23b1
booshwa <bernard.j.conrad@gmail.com>
2025-01-03 15:12:50 -0600

    Updated active menu status styles, removed unnecessary function, formatted index.html and removed unnecessary elements, removed unnecessary imports in style.css

--- functions.js
+++ functions.js
@@ -1,17 +1,4 @@
-//Change color on click
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('#scroll-links a');
-
-    links.forEach(link => {
-      link.addEventListener('click', function() {
-        links.forEach(link => link.classList.remove('active'));
-        this.classList.add('active');
-      });
-    });
-  });
-
 // Modal
-
 document.addEventListener('DOMContentLoaded', function() {
   const links = document.querySelectorAll('#scroll-links a');
 
